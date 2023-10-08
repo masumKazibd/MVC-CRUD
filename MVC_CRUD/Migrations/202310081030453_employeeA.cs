aefe4fde2522f192b96c95ccb5c0b4466f44fe51
masumkazibd <masum.kazi.hantmk@gmail.com>
2023-10-08 16:33:42 +0600

    Database Migration

---
+++ MVC_CRUD/Migrations/202310081030453_employeeA.cs
@@ -0,0 +1,28 @@
+﻿namespace MVC_CRUD.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class employeeA : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Employees",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Grade = c.Int(nullable: false),
+                        Department = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Employees");
+        }
+    }
+}
---
+++ MVC_CRUD/Models/DbModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC_CRUD.Models
+{
+    public enum Grade
+    {
+        M01, M02, M03, M04, M05,
+        Ex01, G01, D01
+    }
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [EnumDataType(typeof(Grade))]
+        public Grade Grade { get; set;}
+        public string Department { get; set; }
+    }
+
+    public class EmployeeDbContext : DbContext
+    {
+        public DbSet<Employee> DbSet { get; set; }
+    }
+}
